73cf8a3989ba254ee77b60a1e6cf2666530f27f4
nickniebaum <nickniebaum@gmail.com>
2013-10-10 09:38:38 -0500

    fix email template links

--- content-backend/application/config/templates/email/final_review_complete.php
+++ content-backend/application/config/templates/email/final_review_complete.php
@@ -2,6 +2,5 @@
 <br />
 Please find the written opinion on the subject assignment here:<br />
 <br />
-<a href="http://www.accidentreview.com/reports/{assignment_id}">http://www.accidentreview.com/reports/{assignment_id}</a></p>
-
-<p><a href="http://www.accidentreview.com">AccidentReview</a> Team</p>
+<a href="http://backend.accidentreview.com/reports/{assignment_id}">http://backend.accidentreview.com/reports/{assignment_id}</a></p>
+<p><a href="http://accidentreview.com">AccidentReview</a> Team</p>
