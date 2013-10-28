d9795251a9302a87f54868f9e99dbb896cd7538f
nickniebaum <nickniebaum@gmail.com>
2013-10-28 15:46:29 -0500

    trying to fix stuff for ssl

--- content-frontend/wp-content/themes/accident-review/header-home.php
+++ content-frontend/wp-content/themes/accident-review/header-home.php
@@ -1,11 +1,13 @@
 <?
 // force ssl
+/*
 if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
     header('Strict-Transport-Security: max-age=31536000');
 } else {
     header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true);
     die();
 }
+*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
