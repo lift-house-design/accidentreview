9fe1a29c8d9ae7f6629b2ec21acc711ad3e84c02
nickniebaum <nickniebaum@gmail.com>
2013-11-05 14:16:40 -0600

    report styling

--- content-backend/application/views/assignments/report.php
+++ content-backend/application/views/assignments/report.php
@@ -24,7 +24,7 @@
 				<td style="vertical-align:top">
 					<label>By</label> 
 				</td>
-				<td style="vertical-align:top">
+				<td style="vertical-align:top;padding-top:8px">
 					<?= $tech['first_name'].' '.$tech['last_name'] ?>
 					<?=( empty($tech['signature']) ? '' : ', '.$tech['signature'] ) ?>
 				</td>
